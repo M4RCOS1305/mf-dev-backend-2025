06421e2f5f26c56fe7b585fd597844209e4ec71b
Marcos Vinicius <marcosvfs13@gmail.com>
2025-10-23 19:18:21 -0300

    Configuração do Banco de Dados e da Tabela veículo

---
+++ mf-dev-backend-2025/Models/Veiculo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_dev_backend_2025.Models
+{
+    [Table("Veiculo")]
+    public class Veiculos
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o nome.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a placa.")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o ano da fabricação.")]
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o ano do modelo.")]
+        public int AnoModelo { get; set; }
+    }
+}
